8cdd4ee38490a3c12eafa2f0ca67b29dc8a693d8
atul2861 <atulverma2861@gmail.com>
2025-01-12 20:47:45 +0530

    push

--- client/src/Services/api.js
+++ client/src/Services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "" });
 
 // Attach token to requests
 API.interceptors.request.use((config) => {
